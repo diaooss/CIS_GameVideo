706415a2b571763e2a6ac5ed69a8d453345fc048
diaooss <1823870397@qq.com>
2013-09-03 14:07:40 +0800

    liu
    corrcet some eeor

--- CIS_Game_video 2/CIS_Game_video/RequestTools.h
+++ CIS_Game_video 2/CIS_Game_video/RequestTools.h
@@ -17,10 +17,13 @@
     NSDictionary * resultDic;//请求后的解析后的结果字典
 }
 @property(nonatomic,assign)id<myHttpRequestDelegate>delegate;//设置代理
+//根据字符串,发起异步请求
+-(void )requestWithUrl_Asynchronous:(NSString *)urlStr;
+//根据字符串,发起同步请求
+-(void )requestWithUrl_Synchronous:(NSString *)urlStr;
 //类方法--根据请求返回的状态值,返回布尔值,以供判断
 +(BOOL)requestReturnYesOrOkWithCheckUrl_Asynchronous:(NSString *)checkUrl;//异步
 +(BOOL)requestReturnYesOrOkWithCheckUrl_Synchronous:(NSString *)checkUrl;//同步
-
 @end
 //定义请求工具的代理,以便进行监控和值回传
 @protocol myHttpRequestDelegate <NSObject>
