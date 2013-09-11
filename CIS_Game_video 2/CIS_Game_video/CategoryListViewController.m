1b5c39ae57f97da48de0863eff6ce12a535d3c1e
diaooss <1823870397@qq.com>
2013-09-11 17:00:27 +0800

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "liu""""""""""""
    This reverts commit 2af28694fc319e19de7cef9bb8ecd52c67dafa6f.

--- CIS_Game_video 2/CIS_Game_video/CategoryListViewController.m
+++ CIS_Game_video 2/CIS_Game_video/CategoryListViewController.m
@@ -42,8 +42,6 @@
 {
     
     [super viewDidLoad];
-    [Tools navigaionView:self leftImageName:@"goBack.png"];
-    _categoryTable = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, 320, 440)];
     [Tools navigaionView:self leftImageName:@"goBack.png" rightImageName:nil title:self.title];
 
     _categoryTable = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, 320, self.view.frame.size.height-44)];
